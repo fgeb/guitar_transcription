831083a856928f83af96b76e0aab9bbbb68bcef8
Frederik Gebhardt <frederik.gebhardt@hotmail.de>
2025-03-31 18:45:23 +0200

    first inference

--- run_inference.py
+++ run_inference.py
@@ -6,6 +6,7 @@ import librosa
 import note_seq
 import seqio
 import t5x
+import t5
 import jax
 import gin
 
@@ -165,7 +166,7 @@ class InferenceModel:
     def preprocess(self, ds):
         pp_chain = [
             functools.partial(
-                t5x.data.preprocessors.split_tokens_to_inputs_length,
+                t5.data.preprocessors.split_tokens_to_inputs_length,
                 sequence_length=self.sequence_length,
                 output_features=self.output_features,
                 feature_key="inputs",
